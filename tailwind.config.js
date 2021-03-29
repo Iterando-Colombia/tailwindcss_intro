56371d2c8e3c6a4fdded91bfa5eda25145626073
Jeferson Sanchez <jesanchez@merqueo.com>
2021-03-29 18:18:14 -0500

    initial files

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./**/*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1da1f2",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
